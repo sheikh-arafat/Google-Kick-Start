341a67fff138b7a3563c8ebd56f5cbe7737df5ed
Sheikh Mohammad Arafat <sheikh.mohammad.arafat@g.bracu.ac.bd>
2022-06-28 15:03:02 +0600

    Delete Sample Problem.py

--- Sample Problem.py
+++
@@ -1,8 +0,0 @@
-cases = int(input())
-
-for c in range(cases) :
-    bag, child = map(int, input().split())
-    a = list(map(int,input().split()))[:bag]
-    totalCan = sum([num for num in a])
-    out = totalCan % child
-    print("Case #"+ str(c+1) +": "+str(out))
